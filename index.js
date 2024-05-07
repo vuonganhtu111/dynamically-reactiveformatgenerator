b7a43ae6c4998134ee61d4ebed8134489ea9eca2
vuonganhtu111 <sameowen69pun0suy@hotmail.com>
2024-05-07 14:22:43 +0700

    hello

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("hi");
+function longestPalindrome(s) {
+  const map = new Map();
+  let hasOdd = false;
+  let result = 0;
+  for (const char of s) {
+    map.set(char, (map.get(char) || 0) + 1);
+  }
+  for (const count of map.values()) {
+    if (count % 2 === 0) result += count;
+    else {
+      result += count - 1;
+      hasOdd = true;
+    }
+  }
+  return hasOdd ? result + 1 : result;
+}
