0ed9fcc9cc566ac0040add5007669d46efe3e4b2
vuonganhtu111 <sameowen69pun0suy@hotmail.com>
2024-05-12 08:33:50 +0700

    update task

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-const word = "hello";
-const reversedWord = word.split("").reverse().join("");
-console.log(reversedWord); // olleh
+function isPrime(n) {
+  if (n <= 1) return false;
+  if (n <= 3) return true;
+  if (n % 2 === 0 || n % 3 === 0) return false;
+  let i = 5;
+  while (i * i <= n) {
+    if (n % i === 0 || n % (i + 2) === 0) return false;
+    i += 6;
+  }
+  return true;
+}
