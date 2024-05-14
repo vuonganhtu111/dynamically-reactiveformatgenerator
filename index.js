454033e8cb7edaa5b1f0f60073b44e823f5887ae
vuonganhtu111 <sameowen69pun0suy@hotmail.com>
2024-05-14 07:50:03 +0700

    aptos task

--- index.js
+++ index.js
@@ -1,22 +1,4 @@
-function minStack() {
-  this.stack = [];
-  this.minStack = [];
-}
-minStack.prototype.push = function (x) {
-  this.stack.push(x);
-  if (
-    this.minStack.length === 0 ||
-    x <= this.minStack[this.minStack.length - 1]
-  )
-    this.minStack.push(x);
-};
-minStack.prototype.pop = function () {
-  if (this.stack.pop() === this.minStack[this.minStack.length - 1])
-    this.minStack.pop();
-};
-minStack.prototype.top = function () {
-  return this.stack[this.stack.length - 1];
-};
-minStack.prototype.getMin = function () {
-  return this.minStack[this.minStack.length - 1];
+const difference = (a, b) => {
+  const s = new Set(b);
+  return a.filter((x) => !s.has(x));
 };
